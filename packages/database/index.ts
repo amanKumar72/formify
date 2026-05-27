ee7db5dec56bc0f221cdc16f5013dd57be287fd6
amanKuamr72 <amankumar72@gmail.com>
2026-05-27 22:26:04 +0530

    feat: update database connection to use neon serverless and adjust dependencies

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -1,7 +1,10 @@
 import "dotenv/config";
-import { drizzle } from "drizzle-orm/node-postgres";
 import { env } from "./env";
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
 
-export const db = drizzle(env.DATABASE_URL);
+const sql = neon(env.DATABASE_URL!);
+
+export const db = drizzle(sql);
 export * from "drizzle-orm";
 export default db;
